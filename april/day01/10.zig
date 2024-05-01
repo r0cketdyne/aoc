12d48ee5546c250deff2ead46f6e0b6c2b829433
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-05-01 14:57:08 +0300

    Yeet!

---
+++ april/day01/10.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
+
+pub fn main()!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const file_path = "puzzleinput.txt";
+    const file = try fs.cwd().openFile(file_path,.{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    const reader = buf_reader.reader();
+
+    var line = std.ArrayList(u8).init(allocator);
+    defer line.deinit();
+
+    const writer = line.writer();
+    var line_no: usize = 1;
+    while (reader.streamUntilDelimiter(writer, '\n', null)) : (line_no += 1) {
+        defer line.clearRetainingCapacity();
+
+        print("{d}: {s}\n",.{ line_no, line.items });
+    } else |err| switch (err) {
+        error.EndOfStream => {}, // Continue on
+        else => return err, // Propagate error
+    }
+}
+

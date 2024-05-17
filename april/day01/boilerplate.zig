e1e3f31b44b75b381f700583a94120513bd812a3
Matthew James Stephenson <matthew.stephenson@uah.edu>
2024-05-17 11:40:37 -0400

    Yeet!

---
+++ april/day01/boilerplate.zig
@@ -0,0 +1,7 @@
+pub fn tokenizeAny(comptime T: type, buffer: []const T, delimiters: []const T) TokenIterator(T, .any) {
+    return .{
+        .index = 0,
+        .buffer = buffer,
+        .delimiter = delimiters,
+    };
+}
